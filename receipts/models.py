375bda3889cfd4d9be2dc5caa19d76ca7bd444b2
MOHAMMAD <m.ayazadeh@gmail.com>
2021-07-21 14:23:32 +0430

    add text of receipts to locale

--- receipts/models.py
+++ receipts/models.py
@@ -1,17 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class Receipt(models.Model):
-    order = models.PositiveIntegerField(verbose_name="number of order's:",
-                                        help_text="add number of order's")
+    order = models.PositiveIntegerField(verbose_name=_("number of order's:"),
+                                        help_text=_("add number of order's"))
 
-    total_price = models.FloatField(verbose_name="total price:")
-    total_discount = models.FloatField(verbose_name="total discount:")
-    final_price = models.FloatField(verbose_name="final price:")
+    total_price = models.FloatField(verbose_name=_("total price:"))
+    total_discount = models.FloatField(verbose_name=_("total discount:"))
+    final_price = models.FloatField(verbose_name=_("final price:"))
     creat_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
-    deleted = models.BooleanField()
+    deleted = models.BooleanField(verbose_name=_("delete"))
 
     def __str__(self):
-        return f'{self.id}# order:{self.order} - final price:{self.final_price}' \
-
+        return f'{self.id}# order:{self.order} - final price:{self.final_price}'
